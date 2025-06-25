b43672aa2532aac023c0d8c03ba49707057d2990
Maciej <maciejadula22@gmail.com>
2025-06-25 22:14:55 +0200

    Aspect update = Replacing @Before and @After with one @Around which does the same but is more concise

--- src/main/java/org/example/driverandfleetmanagementapp/aop/LoggingAspect.java
+++ src/main/java/org/example/driverandfleetmanagementapp/aop/LoggingAspect.java
@@ -2,10 +2,12 @@ package org.example.driverandfleetmanagementapp.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.stereotype.Component;
 import java.util.Arrays;
+
 
 @Aspect
 @Component
@@ -27,31 +29,29 @@ public class LoggingAspect {
     }
 
 
-    @Before("applicationPackagePointcut()")
-    public void logBefore(JoinPoint joinPoint) {
+
+    @Around("applicationPackagePointcut()")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
         String className = joinPoint.getTarget().getClass().getSimpleName();
         String methodName = joinPoint.getSignature().getName();
 
         log.info("Starting {} in class {}", methodName, className);
-
         if (log.isDebugEnabled()) {
             String args = Arrays.toString(joinPoint.getArgs());
             log.debug("Method: {}.{} called with arguments: {}", className, methodName, args);
         }
-    }
 
 
-    @After("applicationPackagePointcut()")
-    public void logAfter(JoinPoint joinPoint) {
-        String className = joinPoint.getTarget().getClass().getSimpleName();
-        String methodName = joinPoint.getSignature().getName();
+        Object result = joinPoint.proceed();
 
         log.info("Completed {} in class {}", methodName, className);
-
         if (log.isDebugEnabled()) {
             log.debug("Method: {}.{} completed execution", className, methodName);
         }
+
+        return result;
     }
+
 
     @AfterThrowing(pointcut = "exceptionPackagePointcut()", throwing = "exception")
     public void logException(JoinPoint joinPoint, Exception exception) {
